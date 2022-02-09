da66f52a45b640c295fd3a6cb3481fb3e9137d31
DanielKhaikin <daniel2003khaikin@gmail.com>
2022-02-09 09:26:54 +0200

    finished class with getters, add song and check if song exists

---
+++ core/album.py
@@ -0,0 +1,30 @@
+from core.song import Song
+from typing import List
+
+
+class Album:
+    def __init__(self, album_id, name, songs: List[Song]):
+        self.album_id = album_id
+        self.name = name
+        self.songs = songs
+
+    def get_album_id(self):
+
+        return self.album_id
+
+    def get_album_name(self):
+
+        return self.name
+
+    def get_songs(self):
+
+        return self.songs
+
+    def add_song(self, new_song: Song):
+
+        self.songs.append(new_song)
+
+    def song_exists(self, song: Song):
+
+        return song in self.songs
+
