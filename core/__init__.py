47d33b7aa9aa3a87ecad9a862ebd7a343706d237
DanielKhaikin <daniel2003khaikin@gmail.com>
2022-02-09 15:35:17 +0200

    finished spotifymusic

---
+++ core/__init__.py
@@ -0,0 +1,6 @@
+from core.spotipymusic import get_artists
+
+
+def get_all_music():
+
+    return get_artists()
---
+++ core/spotipymusic/__init__.py
@@ -0,0 +1,3 @@
+def get_artists():
+    from core.spotipymusic.songsreader import load_songs
+    return load_songs()
