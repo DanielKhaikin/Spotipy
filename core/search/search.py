fa54ea638c0745d2fbf6f3da9d4183e6d37fa5ee
DanielKhaikin <daniel2003khaikin@gmail.com>
2022-02-09 17:37:07 +0200

    get songs from album

--- core/search/search.py
+++ core/search/search.py
@@ -12,12 +12,30 @@ def get_artists_names():
         print(artist.get_artist_name())
 
 
-def get_artist_albums(artist_id):
+def get_artist_albums_names(artist_id):
     for artist in get_all_artists():
         if artist.get_artist_id() == artist_id:
             for album in artist.get_albums():
                 print(album.get_album_name())
 
 
-get_artist_albums("7Ln80lUS6He07XvHI8qqHH")
+def get_all_albums():
+    artists = get_all_artists()
+    albums = []
+    for artist in artists:
+
+        for album in artist.get_albums():
+            albums.append(album)
+
+    return albums
+
+
+def get_songs_from_album(album_id):
+    for album in get_all_albums():
+        if album.get_album_id() == album_id:
+            [print(song.get_song_name()) for song in album.get_songs()]
+
+
+get_songs_from_album("1Y1kv5n86UzcV3dBkkAsSq")
+get_artist_albums_names("7Ln80lUS6He07XvHI8qqHH")
 
