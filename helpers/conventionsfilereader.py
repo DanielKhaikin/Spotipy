ce477b45179feec7258124486f59921b3b873a69
DanielKhaikin <daniel2003khaikin@gmail.com>
2022-02-10 11:08:51 +0200

    added location to conventions file, added func to get location

--- helpers/conventionsfilereader.py
+++ helpers/conventionsfilereader.py
@@ -6,3 +6,10 @@ def get_songs_location():
         data = json.load(conv_file)
 
     return data["songs location"]
+
+
+def get_users_location():
+    with open("C:\\Users\\Daniel\\PycharmProjects\\Spotipy\\helpers\\documents\\conventions.json", 'r') as conv_file:
+        data = json.load(conv_file)
+
+    return data["users location"]
